ffd4217afcb85f8c9a9551eee8daa86f259d02d7
megahon1010 <144912762+megahon1010@users.noreply.github.com>
2025-10-27 17:13:26 +0900

    Update swordRanks array to include new ranks

--- sword_calculator.ts
+++ sword_calculator.ts
@@ -1,9 +1,9 @@
 // sword_calculator.ts
 
-// 剣のランクを定義した配列 (GM+、MR、MR+、GR、GR+を追加)
+// 剣のランクを定義した配列 (GRとGR+を含む)
 const swordRanks = [
   "e", "d", "c", "b", "a", "s", "ss", "g", "n", "n+",
-  "m", "m+", "gm", "gm+", "ugm", "ugm+", "hgm", "hgm+", "r", "r+", "mr", "mr+", "gr", "gr+", "ur", "ur+" // URとUR+を追加
+  "m", "m+", "gm", "gm+", "ugm", "ugm+", "hgm", "hgm+", "r", "r+", "mr", "mr+", "gr", "gr+", "ur", "ur+" 
 ];
 
 /**
