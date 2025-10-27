f055d0a68e956bc7d99ec23e25d35e64256ad530
megahon1010 <144912762+megahon1010@users.noreply.github.com>
2025-10-27 17:23:44 +0900

    Create sword_ranks.ts

---
+++ sword_ranks.ts
@@ -0,0 +1,10 @@
+// sword_ranks.ts
+
+/**
+ * 剣のランクを定義した配列。
+ * ランクを追加する場合は、この配列の末尾に追記するだけでOKです。
+ */
+export const swordRanks = [
+  "e", "d", "c", "b", "a", "s", "ss", "g", "n", "n+",
+  "m", "m+", "gm", "gm+", "ugm", "ugm+", "hgm", "hgm+", "r", "r+", "mr", "mr+", "gr", "gr+", "ur", "ur+" // UR, UR+を含む
+];
